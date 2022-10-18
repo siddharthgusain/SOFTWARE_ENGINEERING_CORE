5d00300463c33afd5e8238669760eb06f8264323
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-10-18 20:40:55 +0530

    liskov substitution principle

---
+++ 3.OOPS/30.9.Liskov_substitution_principle.cpp
@@ -0,0 +1,7 @@
+/*
+if a class B is subtype of class A , then we should be able to replace object of A
+with B without breaking the behaviour of the program
+every child class of a parent should follow all the methods of its parent.
+
+very usefull to write inheritance in write way
+*/
