441dc0cd1d81c17817ecf3c217378c42301c6858
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2024-12-28 21:32:25 +0530

    HTTP Messages

---
+++ 6.SYSTEM_DESIGN/LLD/Parking_lot/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Parking Lot System Simulation Start.....");
+        // Add vehicles -> Fill Parking Spot
+        // Parking Ticket -> Start time 
+
+
+        // Remove Vehicles -> Empty Parking Slot
+        // Parking Ticket close -> End Time
+        // Parking Ticket Price
+    }
+}
